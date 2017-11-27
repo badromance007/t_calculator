2e2f1d80728f2009a2c653ab875e87cf9c82c1a8
badromance007 <nguyenpeter.edu@gmail.com>
2017-11-28 04:31:12 +0700

    Initial Commit

---
+++ calculator1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator1
+//
+//  Created by Nguyen Trong Tuong on 11/28/17.
+//  Copyright © 2017 tuongpro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
