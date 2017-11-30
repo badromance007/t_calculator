1741a407ab80128b77d121e4a94df556706c7b01
badromance007 <nguyenpeter.edu@gmail.com>
2017-11-30 15:07:17 +0700

    Add commas to separate thousand units.

--- calculator1/ViewController.swift
+++ calculator1/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var numberString:String = ""
+    
+    @IBAction func number(_ sender: UIButton) {
+        
+        numberString = numberString + String(sender.tag)
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = NumberFormatter.Style.decimal
+        let formattedNumber = numberFormatter.string(from: NSNumber(value: Double(numberString)!))!
+        
+        label.text = formattedNumber
+        label.numberOfLines = 3
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor=0.4
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
